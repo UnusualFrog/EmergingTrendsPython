ab92ff9374916c4767ac00df71423b5f8b26fdf5
Forward <Julia.Forward@k202domain.com>
2025-01-17 10:20:18 -0330

    first commit

---
+++ test/test.py
@@ -0,0 +1,10 @@
+import numpy as np
+np.random.seed(0)  # seed for reproducibility
+
+x1 = np.random.randint(10, size=6)  # One-dimensional array
+x2 = np.random.randint(10, size=(3, 4))  # Two-dimensional array
+x3 = np.random.randint(10, size=(3, 4, 5))  # Three-dimensional array
+
+print("x3 ndim: ", x3.ndim)
+print("x3 shape:", x3.shape)
+print("x3 size: ", x3.size)
